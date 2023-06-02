1bd5c5d7791a373fd74f4ca0ea9641384fb84cdd
João Lucas <joaolucassm0704@gmail.com>
2023-06-02 12:04:55 -0300

    Camada de services e registro de componentes

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -15,7 +15,7 @@ import com.educandoweb.course.repositories.UserRepository;
 public class TestConfig implements CommandLineRunner{
 
 	@Autowired
-	private UserRepository userRepository;
+	private UserRepository repository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -23,6 +23,6 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "9888888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "9777777777", "123456");
 		
-		userRepository.saveAll(Arrays.asList(u1, u2));
+		repository.saveAll(Arrays.asList(u1, u2));
 	}
 }
